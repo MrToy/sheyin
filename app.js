c463f9e0a32c19cbd69a4630418a0d5b15c7494e
unknown <docker001@github.com>
2015-12-10 19:02:08 +0800

    test

---
+++ app.js
@@ -0,0 +1,35 @@
+var koa    = require('koa'),
+	router = require('koa-router')(),
+	logger = require('koa-logger'),
+	mongo  = require('koa-mongo'),
+	serve  = require('koa-static'),
+	gzip  = require('koa-gzip'),
+	cache  = require('koa-static-cache'),
+	path   = require('path'),
+	parse  = require('co-body'),
+	session = require('koa-session-store'),
+	ObjectId = require('mongodb').ObjectId,
+	routerDB = require('./router/db'),
+	routerUser =require('./router/user')
+var app = koa()
+app.keys=['imtoy']
+router
+	.use('/user',routerUser.routes(),routerUser.allowedMethods())
+	.use('/db',routerDB.routes(),routerDB.allowedMethods())
+app
+	.use(mongo({
+		host:process.env["MONGODB_PORT_27017_TCP_ADDR"]||'localhost',
+		port:process.env["MONGODB_PORT_27017_TCP_PORT"]||27017,
+		user:process.env["MONGODB_USERNAME"]||'',
+		pass:process.env["MONGODB_PASSWORD"]||' ',
+		db:process.env["MONGODB_INSTANCE_NAME"]||'test'
+	}))
+	.use(session({store:"cookie"}))
+	.use(logger())
+	.use(gzip())
+	.use(router.routes())
+	.use(router.allowedMethods())
+	.use(cache("dest"))
+	.use(serve("dest"))
+	.listen(80)
+console.log("start app success on port 80")
---
+++ router/db.js
@@ -0,0 +1,45 @@
+var router = require('koa-router')()
+var ObjectId = require('mongodb').ObjectId
+var parse  = require('co-body')
+function auth(except){
+	return function *(next){
+		if((except||[]).indexOf(this.params.coll)==-1){
+			if(!this.session.user||this.session.user.group!="admin"){
+				return this.body={error:"无权限"}
+			}
+		}
+		yield next
+	}
+}
+router
+	.get('/:coll',auth(["news","jiedai","config"]),function*(){
+		var res=this.mongo.collection(this.params.coll)
+		var total=yield res.count()
+		var search={}
+		if(this.query.key&&this.query.value){
+			search[this.query.key]=this.query.value
+		}
+		res=res.find(search)
+		if(this.query.sort){
+			var sort={}
+			sort[this.query.sort]=parseInt(this.query.sortdir||1)
+			res=res.sort(sort)
+		}
+		if(this.query.skip)res=res.skip(parseInt(this.query.skip))
+		if(this.query.limit)res=res.limit(parseInt(this.query.limit))
+		res=yield res.toArray()
+		this.body={data:res,total:total}
+	})
+	.get('/:coll/:id',auth(["news","jiedai","config"]),function*(){
+		this.body=yield this.mongo.collection(this.params.coll).findOne({_id:ObjectId(this.params.id)})
+	})
+	.post('/:coll',auth(),function*(){
+		this.body=yield this.mongo.collection(this.params.coll).insert(yield parse.json(this))
+	})
+	.del('/:coll/:id',auth(),function*(){
+		this.body=yield this.mongo.collection(this.params.coll).remove({_id:ObjectId(this.params.id)})
+	})
+	.put('/:coll/:id',auth(),function*(){
+		this.body=yield this.mongo.collection(this.params.coll).update({_id:ObjectId(this.params.id)},{$set:yield parse.json(this)})
+	})
+module.exports=router
